4026f137bcf340d1ac7ccffcb6113b22d4b479f0
Saumya Pratap Singh <118092711+Saumya101203@users.noreply.github.com>
2025-07-08 17:01:39 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -28,8 +28,8 @@ const corsOptions = {
   credentials: true, // Allow cookies/auth headers
 };
 
-app.use(cookieParser());
 app.use(cors(corsOptions));
+app.use(cookieParser());
 
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json({ limit: '16kb' }));
